9cee25a4a65e04523358cefaf23cde1f401775c7
Mike Sheets <wmichaelsheets@gmx.com>
2024-09-09 14:36:57 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleBtnClick = () => {
+    setCount(count +1)
+ }
+  
+  return (
+    <>
+      <h1>Hello!</h1>
+      <div>This is amazing!</div>
+      <button className="btn-secondary" onClick={handleBtnClick}>
+        Click me!
+      </button>
+      <div>Count: {count} </div>
+    </>
+  )
+}
+
+
